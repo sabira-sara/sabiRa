ae240215d5681d6c121de5503babcbeb0b8d0d76
sabira-sara <sabiraahmed751@gmail.com>
2025-04-24 21:11:22 +0600

    Add university life percentage and logical operations

---
+++ HOMEWORK R.R
@@ -0,0 +1,9 @@
+year_started_univ <- 2018
+year_born <- 2000
+current_year <- 2025
+
+years_in_univ <- current_year - year_started_univ
+age <- current_year - year_born
+
+life_pct_in_univ <- (years_in_univ / age) * 100
+life_pct_in_univ
